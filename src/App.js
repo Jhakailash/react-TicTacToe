8a3f4f16b48659f0a13c28cf6770e6f5780ab913
Jhakailash <kailashjhaji7@gmail.com>
2022-06-06 13:54:43 +0530

    tictactoe

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import logo from './logo.svg';
+import './App.css';
+
+
+import TikTakToe from './components/TikTakToe';
+
+
+function App() {
+
+  return (
+    <div>
+  <TikTakToe/>
+  
+     
+    </div>
+  );
+}
+
+export default App;
